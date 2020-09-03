77dfcb5901a3136f209860a6c40bc2466ef55792
Jan Gevaert <jan.gevaert@gmail.com>
2020-09-03 06:55:45 -0600

    Initial commit

---
+++ src/countEvens.java
@@ -0,0 +1,12 @@
+public class countEvens {
+  public int countEvens(int[] nums) {
+    int count = 0;
+    for (int num : nums) {
+      if (num % 2 == 0) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+}
